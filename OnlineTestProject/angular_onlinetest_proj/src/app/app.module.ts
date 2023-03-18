d1495c70fae4a5c0fafd54c89ddea5b7644245f2
SoujiMad1310 <soujanya.madanu@gamil.com>
2023-03-18 14:01:24 +0530

    Second commit of project

--- OnlineTestProject/angular_onlinetest_proj/src/app/app.module.ts
+++ OnlineTestProject/angular_onlinetest_proj/src/app/app.module.ts
@@ -6,17 +6,20 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { QuestionsComponent } from './questions/questions.component';
-
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     WelcomeComponent,
-    QuestionsComponent
+    QuestionsComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
